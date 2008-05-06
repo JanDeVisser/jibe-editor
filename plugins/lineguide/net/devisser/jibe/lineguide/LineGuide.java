c561bff9492d9fd82b35d7efe9b01fd535ac8bd3
jan.devisser <jan.devisser@38d4d7d8-4435-0410-b88e-411785732dbb>
2008-05-06 01:08:37 +0000

    xx

---
+++ plugins/lineguide/net/devisser/jibe/lineguide/LineGuide.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.devisser.jibe.lineguide;
+
+import com.trolltech.qt.gui.QColor;
+import com.trolltech.qt.gui.QFont;
+import com.trolltech.qt.gui.QFontMetrics;
+import com.trolltech.qt.gui.QPaintEvent;
+import com.trolltech.qt.gui.QPainter;
+import net.devisser.jibe.Config;
+import net.devisser.jibe.Jibe;
+import net.devisser.jibe.JibeTextEdit;
+import net.devisser.jibe.Plugin;
+import net.devisser.jibe.PluginConfig;
+import net.devisser.jibe.TextEditListener;
+
+/**
+ *
+ * @author jan
+ */
+public class LineGuide implements Plugin, TextEditListener {
+
+  private int m_guidepx = 0;
+  
+  public void initialize(Jibe jibe, PluginConfig config) {
+    QFont font = new QFont();
+    font.setFamily(Config.getProperty("jibe.editor.font"));
+    font.setFixedPitch(true);
+    font.setPointSize(Config.getIntProperty("jibe.editor.fontsize"));
+    
+    int guideat = Config.getIntProperty("jibe.editor.guide.column");
+    if (guideat > 0) {
+      StringBuffer sb = new StringBuffer();
+      while (sb.length() < guideat) sb.append('W');
+      QFontMetrics fm = new QFontMetrics(font);
+      m_guidepx = fm.width(sb.toString()) + 2;
+    } else {
+      m_guidepx = 0;
+    }
+    jibe.getBufferManager().registerTextEditListener(this);
+  }
+  
+  public void unload(Jibe jibe) {
+    jibe.getBufferManager().removeTextEditListener(this);    
+  }
+
+  public void paintEvent(QPaintEvent event, JibeTextEdit te) {
+    if (m_guidepx > 0) {
+      QPainter p = new QPainter();
+      p.begin(te.viewport());
+      p.setPen(new QColor(Config.getProperty("jibe.editor.guide.color")));
+      p.drawLine(m_guidepx, 0, m_guidepx, te.height());
+      p.end();
+    }
+  }
+}
+
